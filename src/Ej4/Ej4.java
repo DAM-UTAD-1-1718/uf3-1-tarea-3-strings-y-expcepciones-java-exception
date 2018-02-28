f78c054254bcff2dd8d57fc0c26fd56528380767
Sara Correas <32343542+sarascript@users.noreply.github.com>
2018-02-28 10:44:58 +0100

    Update Ej4.java

--- src/Ej4/Ej4.java
+++ src/Ej4/Ej4.java
@@ -15,16 +15,16 @@ public class Ej4 {
 		Scanner in = new Scanner(System.in);
 		ArrayList<Integer> numArray = new ArrayList<Integer>();
 		int resultado = 0;
-		//Mientras no se hayan introducido 5 números, se seguirá pidiendo uno y comprobando que es numérico. Así se consigue que el método no termine con la excepción, sino que siga pidiéndolos.
+		//Mientras no se hayan introducido 5 nÃºmeros, se seguirÃ¡ pidiendo uno y comprobando que es numÃ©rico. AsÃ­ se consigue que el mÃ©todo no termine con la excepciÃ³n, sino que siga pidiÃ©ndolos.
 		while (numArray.size() < 5) {
 			try {
-		//Utilizamos el parseInt para que suceda nuestra excepción en lugar de que el método de un programa por InputMismatch
+		//Utilizamos el parseInt para que suceda nuestra excepciÃ³n en lugar de que el mÃ©todo de un error por InputMismatch
 					numArray.add(Integer.parseInt(in.next()));
 			} catch (NumberFormatException e) {
-				System.out.println("Se han introducido caracteres no numéricos. Introduzca un número.");
+				System.out.println("Se han introducido caracteres no numÃ©ricos. Introduzca un nÃºmero.");
 			}
 		}
-		//Fórmula en Java 8 para realizar la sumatoria de un ArrayList
+		//FÃ³rmula en Java 8 para realizar la sumatoria de un ArrayList
 		resultado = numArray.stream().mapToInt(Integer::intValue).sum();
 		System.out.println("El resultado de la suma es: " + resultado);
 		return resultado;
